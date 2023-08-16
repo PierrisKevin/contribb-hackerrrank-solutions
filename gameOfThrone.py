c97854a67e1577d413211032d77ec0d51ba8cf60
AliciaRakotomamonjy <80815632+AliciaRakotomamonjy@users.noreply.github.com>
2023-08-16 11:59:06 +0300

    finished game of throne

--- gameOfThrone.py
+++ gameOfThrone.py
@@ -1,5 +1,9 @@
 from itertools import combinations
-def isAnagram(s):
-    allCombin = combinations(s, 5)
-    return list(allCombin)
+def gameOfThrones(s):
+    res = []
+    data = dict(Counter(s))
+    for i in data:
+        if data[i]%2!=0:res.append(i)
+    if len(res)>1:return "NO"
+    return "YES"
 print(isAnagram("kevin"))
